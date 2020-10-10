3965366d613d042bb43ddef76d01800131737867
Brian Ma <jingjerma@gmail.com>
2020-10-10 14:28:17 -0700

    Finished handling responses from path /links "

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+    /*event.respondWith(new Promise((resolve, reject) => {
+        // Get url of request
+        let url = new URL(event.request.url);
+        // Request handler to respond to the path '/links'; returns data as JSON
+        if (url.pathname == '/links') {
+            resolve(handleLinkRequests(events.request));
+        }
+    })) */
+    event.respondWith(handleLinkRequests(event.request));
 })
+
+// LinkTree data
+const data = [
+    {name : "LinkedIn", url : "https://www.linkedin.com/in/brian-j-ma/"},
+    {name : "GitHub", url : "https://github.com/bjma/"},
+    {name : "Reddit", url : "https://www.reddit.com/"},
+]
+
 /**
- * Respond with hello worker text
- * @param {Request} request
+ * Handles requests from path '/links' and returns array as JSON.
+ * Includes header displaying attribute 'Content-Type'.
  */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+async function handleLinkRequests(request) { 
+    try {
+        return new Response(JSON.stringify(data), {
+            headers: { 'Content-Type' : 'application/json'}
+        });
+    } catch (err) {
+        console.log('Exit with error code:', err);
+    }
 }
